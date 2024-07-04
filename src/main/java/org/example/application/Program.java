8a39fc1e6f914a4440d9222a2ea13db289369abf
Jess Santos <jessica.js1306@gmail.com>
2024-07-04 14:32:53 -0300

    Department entity class

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,10 +1,10 @@
-package org.example;
+package org.example.application;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class Program
 {
     public static void main( String[] args )
     {
