ec5ceed937034fd5c5586f17a16484df72844ac6
Jess Santos <jessica.js1306@gmail.com>
2024-07-04 14:38:03 -0300

    Seller entity class

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,13 +1,23 @@
 package org.example.application;
 
+import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class Program
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
